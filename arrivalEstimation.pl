53e1b13c3c68d449ca190bc7a003d9b3e39698cb
Thomas <thomas@geenbs.nl>
2017-08-22 16:15:25 +0200

    Did some input checking and usability improvements

--- arrivalEstimation.pl
+++ arrivalEstimation.pl
@@ -1,13 +1,14 @@
 #!/usr/bin/perl
 #arrivalEstimation.pl
 #Just a quick and dirty little tool for arrival time estimation
+use Scalar::Util qw(looks_like_number);
 use strict;
 use warnings;
 
 sub doCalculation{
+    my $depTime = shift;
     my $nm = shift;
     my $avgKts = shift;
-    my $depTime = shift;
 
     my $nmPerDay = $avgKts * 24;
     my $tripTimeInHours = $nm / $avgKts;
@@ -15,6 +16,7 @@ sub doCalculation{
     my $tripTimeInDays = $tripTimeInHours / 24;
     my $arrivalTime = ($tripTimeInHours % 24) - (24 - $depTime);
     
+    print("Departure: $depTime:00\n");
     print("nmPerDay: $nmPerDay\n");
     print("tripTimeInHours: $tripTimeInHours\n");
     print("tripTimeInDays: $tripTimeInDays\n");
@@ -22,19 +24,30 @@ sub doCalculation{
 }
 
 
-my $nm = 165;
-my $avgKts = 5;
+my $nm;
+my $avgKts;
 my $depTime = 20;
 
-print("Total tip nautical miles: ");
-$nm = <STDIN>;
+while(!looks_like_number($nm)){
+    print("Total trip nautical miles: ");
+    $nm = <STDIN>;
+}
 
 while(1){
-    print("\nEnter new departure time (whole 24 hr): ");
-    $depTime = <STDIN>;
-    print("Avg kts: ");
-    $avgKts = <STDIN>;
+    print("\nEnter new departure time (whole 24 hr, def: $depTime): ");
+    my $depTimeTmp = <STDIN>;
+    if(looks_like_number($depTimeTmp)){
+        $depTime = $depTimeTmp;
+    }
+
+    while(!looks_like_number($avgKts)){
+        print("Avg kts: ");
+        $avgKts = <STDIN>;
+    }
     chomp($depTime, $avgKts);
     chomp($avgKts);
-    &doCalculation($nm, $avgKts, $depTime);
+    
+    &doCalculation($depTime, $nm, $avgKts, $depTime);
+
+    $avgKts = "";
 }
